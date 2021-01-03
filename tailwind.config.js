824deb128ab98c4d825770dba6258168e8ef8ddb
vitorserrano <vitorcirinoserrano@gmail.com>
2021-01-02 22:49:13 -0300

    :heavy_plus_sign: chore: Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif'],
+      },
+      colors: {
+        background: '#121214',
+        backgroundSecondary: '#202024',
+        label: '#e1e1e6',
+        primary: '#8257e6',
+        secondary: '#05D361',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
